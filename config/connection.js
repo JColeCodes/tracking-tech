4d185da26086e700a4b76f557a76360fbd8b25f2
Jennifer Cole <capauldi@gmail.com>
2023-05-13 23:50:22 -0700

    Update connection.js to include process.env.MYSQL_URL

--- config/connection.js
+++ config/connection.js
@@ -5,6 +5,8 @@ let sequelize;
 
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else if (process.env.MYSQL_URL) {
+  sequelize = new Sequelize(process.env.MYSQL_URL);
 } else {
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD,
     {
